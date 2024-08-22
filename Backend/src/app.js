38e7e8a62a19af222739701ae8a6bc9c76d6d9aa
Randy <randy26smc@gmail.com>
2024-08-21 19:11:36 -0600

    Creacion de modelo, controllador y rutas para marcas, se crea, actualiza, borra, obtienen nombres de marcas

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,7 @@ const jwtValidacion = require('./middlewares/jwtValidacion');
 const usuariosRoutes = require('./routes/usuarios.routes');
 const adminstracionRoutes = require('./routes/administracion.routes');
 const utilsroutes = require('./routes/utils.routes');
+const marcasRoutes = require('./routes/marcas.routes')
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,8 +20,10 @@ app.get('/',(req,res)=>{
     res.send('Hello from Backend API')
 });
 app.use(utilsroutes);
+app.use(marcasRoutes)
 
 app.use(usuariosRoutes);
 app.use(jwtValidacion,adminstracionRoutes);
+
 
 module.exports = app;
---
+++ Backend/src/models/marca.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../configs/database.configs');
+
+const Marca = sequelize.define(
+    "Marca",
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombreMarca:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notNull:{
+                    msg: "El nombre de la marca no puede ser nulo",
+                },
+                notEmpty:{
+                    msg:'El nombre de la marca no puede estar vacio',
+                },
+            },
+        },
+    },
+    {
+        schema: "catalogo",
+        tableName: "marca"
+    }
+);
+
+module.exports = Marca;
