420679e9982f3068dbfbafe54fdb8caecaeb4ece
AlexisOl <oleoalexis@gmail.com>
2024-09-11 17:12:16 -0600

    recuperacion de contrasenia

--- Frontend/src/app/auth/auth-routing.module.ts
+++ Frontend/src/app/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { loginGuard } from './guard/login.guard';
+import { RecuperarContraseniaComponent } from './pages/recuperar-contrasenia/recuperar-contrasenia.component';
 
 const routes: Routes = [
   {
@@ -16,12 +17,16 @@ const routes: Routes = [
       {
         path: 'register',
         component: RegisterComponent,
-        canActivate: [loginGuard]
+        canActivate: [loginGuard],
+      },
+      {
+        path: 'recuperarContrasenia',
+        component: RecuperarContraseniaComponent,
       },
       {
         path: '**',
         component: LoginComponent,
-        canActivate: [loginGuard]
+        canActivate: [loginGuard],
       },
     ],
   },
--- Frontend/src/app/auth/pages/recuperar-contrasenia/recuperar-contrasenia.component.ts
+++ Frontend/src/app/auth/pages/recuperar-contrasenia/recuperar-contrasenia.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recuperar-contrasenia',
   templateUrl: './recuperar-contrasenia.component.html',
-  styleUrls: ['./recuperar-contrasenia.component.css']
+  styleUrls: ['./recuperar-contrasenia.component.css'],
 })
 export class RecuperarContraseniaComponent {
+  servicioAuth = inject(AuthService);
+  correoElectronico!: string;
+  codigo!: string;
+  sinCodigo: boolean = true;
+  generarContrasenia: boolean = false;
 
+  formulario = new FormGroup({
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    codigo: new FormControl('', [Validators.required]),
+    nuevaContrasenia: new FormControl('', [
+      Validators.required,
+
+      Validators.minLength(8),
+    ]),
+    confirmarContrasenia: new FormControl('', [
+      Validators.required,
+
+      Validators.minLength(8),
+    ]),
+  });
+  obtener2FA() {
+    (this.correoElectronico = this.formulario.get('correo')?.value || ''),
+      this.servicioAuth
+        .generarCodigo(this.correoElectronico)
+        .subscribe((codigo: any) => {
+          if (codigo.ok) {
+            this.sinCodigo = false;
+            console.log(codigo);
+          }
+        });
+  }
+
+  verificar2FA() {
+    this.codigo = this.formulario.get('codigo')?.value || '';
+    this.servicioAuth
+      .verificar(this.correoElectronico, this.codigo)
+      .subscribe((codigo: any) => {
+        console.log(codigo);
+        if (codigo.ok) {
+          this.generarContrasenia = true;
+        }
+      });
+  }
+
+  cambioCredencial() {
+    const password = this.formulario.get('nuevaContrasenia')?.value || '';
+    const repitePassword =
+      this.formulario.get('confirmarContrasenia')?.value || '';
+
+    this.servicioAuth
+      .cambioCredenciales(password, repitePassword, this.correoElectronico)
+      .subscribe((valor) => {
+        console.log(valor);
+      });
+  }
 }
